6f110c40d3d6057f58b37d2b855071c42590b10f
Amine Tayani <amine.tayanipro@gmail.com>
2023-10-05 17:14:30 +0100

    test: add create account form test

--- src/components/auth/create-account/create-account.test.tsx
+++ src/components/auth/create-account/create-account.test.tsx
@@ -1,8 +1,32 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { CreateAccountForm } from './form';
+import '@testing-library/jest-dom/';
 
-describe('Tests for CreateAccount form component', () => {
-  test('renders CreateAccount component', () => {
+describe('CreateAccountForm', () => {
+  it('should render create account form with all fields', () => {
     render(<CreateAccountForm />);
+
+    expect(screen.getByPlaceholderText('Your first Name')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Your last Name')).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText('Your email address')
+    ).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText('Select your country')
+    ).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText('Your LinkedIn profile')
+    ).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Your password')).toBeInTheDocument();
+
+    expect(screen.getByRole('button')).toHaveTextContent('Continue');
+  });
+
+  it('should render Already have an account link', () => {
+    render(<CreateAccountForm />);
+
+    expect(screen.getByText('Already have an account?')).toBeInTheDocument();
+
+    expect(screen.getByRole('link')).toHaveTextContent('Login');
   });
 });
