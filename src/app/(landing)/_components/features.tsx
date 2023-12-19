e034cb302502fdba7ddb392f391aaafed4bba211
Amine Tayani <amine.tayanipro@gmail.com>
2023-12-19 15:16:53 +0100

    feat(landing): edit hero section content

--- src/app/(landing)/_components/features.tsx
+++ src/app/(landing)/_components/features.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Balancer } from 'react-wrap-balancer';
 import FeatureCard from './feature-card';
 import {
   BarChart2,
@@ -16,19 +15,20 @@ export default function Features() {
     <section className='relative overflow-hidden' id='features'>
       <div className='mx-auto my-32 grid max-w-[1400px] items-center justify-center px-4 sm:px-24 md:px-4 lg:px-24'>
         <div className='flex flex-col items-center justify-center gap-16'>
-          <div className='mx-auto mt-1 flex max-w-[666px] flex-col space-y-6 text-center sm:px-0'>
-            <div className='mb-6 bg-gradient-to-b from-orange-400 via-orange-600 to-pink-600 bg-clip-text font-semibold uppercase leading-none tracking-widest text-transparent'>
-              Features
-            </div>
-
-            <h1 className=' text-4xl font-bold leading-[1.3] tracking-[-0.022px] text-primary'>
-              <Balancer>Onyx is your personal job application tracker</Balancer>
-            </h1>
-            <p className='text-lg leading-[1.7] tracking-[-0.014px] text-muted-foreground'>
-              <Balancer>
-                Introducing Onyx – the new app for managing your job
-                applications with precision and ease.
-              </Balancer>
+          <div className='mx-auto mt-8 max-w-md text-center sm:max-w-xl'>
+            <h2 className='font-display text-4xl font-extrabold leading-tight text-primary sm:text-5xl sm:leading-tight'>
+              <span className='bg-gradient-to-r from-amber-500 to-orange-600 bg-clip-text text-transparent'>
+                Powerful
+              </span>{' '}
+              features for{' '}
+              <span className='bg-gradient-to-r from-amber-500 to-orange-600 bg-clip-text text-transparent'>
+                modern
+              </span>{' '}
+              marketing teams
+            </h2>
+            <p className='mt-5 text-muted-foreground/80 sm:text-lg'>
+              Onyx.com is more than just a job tracker. It empowers you with
+              powerful features so you can land jobs sooner.
             </p>
           </div>
           <div className='relative z-10 grid w-full gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 [&>*:nth-child(3)]:hidden md:[&>*:nth-child(3)]:block'>
--- src/app/(landing)/_components/hero.tsx
+++ src/app/(landing)/_components/hero.tsx
@@ -6,55 +6,19 @@ import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <header className='mx-auto mb-10 mt-12 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0'>
-      <a
-        className='group mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.1)] backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'
-        href='/blog/introducing-new-analytics'
-      >
-        <p className='text-sm font-semibold text-gray-700 [text-wrap:balance]'>
-          Introducing New &amp; Improved Analytics for Dub
-        </p>
-        <div className='group relative flex items-center'>
-          <svg
-            className='absolute -ml-1 h-3.5 w-3.5 transition-all group-hover:translate-x-1 group-hover:opacity-0'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-            width='16'
-            height='16'
-          >
-            <path
-              fill-rule='evenodd'
-              d='M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z'
-            ></path>
-          </svg>
-          <svg
-            className='absolute -ml-1 h-3.5 w-3.5 text-black opacity-0 transition-all group-hover:translate-x-1 group-hover:opacity-100'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-            width='16'
-            height='16'
-          >
-            <path
-              fill-rule='evenodd'
-              d='M8.22 2.97a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06l2.97-2.97H3.75a.75.75 0 010-1.5h7.44L8.22 4.03a.75.75 0 010-1.06z'
-            ></path>
-          </svg>
-        </div>
-      </a>
-      <h1 className='mt-5 font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]'>
-        Short Links With
+    <header className='mx-auto mb-10 mt-28 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0'>
+      <h1 className=' font-display text-4xl font-extrabold leading-[1.15] text-primary sm:text-6xl sm:leading-[1.15]'>
+        Unleash Your
         <br />
         <span className='bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent'>
-          Superpowers
+          Career Potential
         </span>
       </h1>
-      <h2 className='mt-5 text-gray-600 sm:text-xl'>
+      <h2 className='mt-5 text-muted-foreground/80 sm:text-xl'>
         Say goodbye to the chaos of endless spreadsheets. Onyx empowers you to
         take control of your job search.
       </h2>
-      <div className='hidden lg:flex lg:items-center lg:space-x-3'>
+      <div className='mx-auto mt-10 flex max-w-fit space-x-4'>
         <Link
           className='rounded bg-hero px-6 py-2 text-sm font-medium text-primary transition-colors hover:bg-hero/80'
           href='/signup'
