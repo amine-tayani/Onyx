0cfa8bba49eaf3ac0918dff585f79c74ec3590fb
Amine Tayani <amine.tayanipro@gmail.com>
2023-12-24 16:36:10 +0100

    feat: hide spotlight effect on mobile

--- src/app/(landing)/_components/feature-card.tsx
+++ src/app/(landing)/_components/feature-card.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { motion, useMotionTemplate, useMotionValue } from 'framer-motion';
 import { type LucideIcon } from 'lucide-react';
 import { cn } from '@/lib/cn';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 interface CardProps {
   title: string;
@@ -15,6 +16,7 @@ interface CardProps {
 
 export default function FeatureCard({ icon, title, description }: CardProps) {
   const MotionCard = motion(Card);
+  const isMobile = useIsMobile();
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
 
@@ -23,6 +25,7 @@ export default function FeatureCard({ icon, title, description }: CardProps) {
     clientX,
     clientY,
   }: ReactMouseEvent) => {
+    if (isMobile) return;
     const { left, top } = currentTarget?.getBoundingClientRect();
 
     mouseX.set(clientX - left);
@@ -34,43 +37,46 @@ export default function FeatureCard({ icon, title, description }: CardProps) {
     <MotionCard
       onMouseMove={handleMouseMovement}
       className={cn(
-        ' group relative w-full overflow-hidden border border-muted-foreground/20 bg-muted transition duration-300'
+        ' group relative w-full cursor-pointer overflow-hidden border border-muted-foreground/20 bg-muted transition duration-300'
       )}
     >
       <motion.div
+        className='pointer-events-none absolute -inset-px opacity-0 transition duration-300 group-hover:opacity-100'
         style={{
+          boxShadow: ' 0 4px 30px rgba(0, 0, 0, 0.1)',
+          filter: 'blur(5px)',
           background: useMotionTemplate`
             radial-gradient(
-              650px circle at ${mouseX}px ${mouseY}px,
-              rgba(10, 0, 10, 0.2),
-              transparent 80%
+              300px circle at ${mouseX}px ${mouseY}px,
+              rgba(10, 10, 10, .05),
+              rgba(10, 10, 10, .05),
+              rgba(10, 10, 10, .05),
+              rgba(10, 10, 10, .05),              
+              transparent 50%
             )
           `,
         }}
-      >
-        <CardHeader className='pb-3'>
-          <CardTitle>
-            <div className=''>
-              <div>
-                {Icon ? (
-                  <Icon
-                    strokeWidth={1.5}
-                    className={cn('mr-3 h-8 w-8 text-neutral-400')}
-                  />
-                ) : null}
-              </div>
-              <h1 className='mt-3 text-base font-normal text-primary'>
-                {title}
-              </h1>
+      />
+      <CardHeader className='pb-3'>
+        <CardTitle>
+          <div className=''>
+            <div>
+              {Icon ? (
+                <Icon
+                  strokeWidth={1.5}
+                  className={cn('mr-3 h-8 w-8 text-neutral-400')}
+                />
+              ) : null}
             </div>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className='text-sm font-light text-neutral-400 sm:text-base sm:leading-7'>
-            {description}
-          </p>
-        </CardContent>
-      </motion.div>
+            <h1 className='mt-3 text-base font-normal text-primary'>{title}</h1>
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className='text-sm font-light text-neutral-400 sm:text-base sm:leading-7'>
+          {description}
+        </p>
+      </CardContent>
     </MotionCard>
   );
 }
