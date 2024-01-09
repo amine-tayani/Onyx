469409aec90ae0ff74f45184d9de7600ae33bac9
Amine Tayani <amine.tayanipro@gmail.com>
2024-01-09 18:51:27 +0100

    feat: add user sessions in settings page

--- src/app/settings/security/page.tsx
+++ src/app/settings/security/page.tsx
@@ -4,10 +4,12 @@ import { SecurityForm } from './security-form';
 export default function SecurityPage() {
   return (
     <div className='space-y-6'>
-      <div>
-        <h3 className='text-lg font-semibold tracking-wide'>Security</h3>
+      <div className='ml-4'>
+        <h3 className='text-lg font-semibold tracking-wide'>
+          Security & Access
+        </h3>
         <p className='text-sm text-muted-foreground'>
-          Select the kinds of notifications you get about your applications.
+          Keep your account secure
         </p>
       </div>
       <Separator />
