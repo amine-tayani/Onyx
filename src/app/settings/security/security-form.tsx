520117aff7460127b45f5d65765694bcc0e96b1e
Amine Tayani <amine.tayanipro@gmail.com>
2024-01-09 19:49:26 +0100

    feat: complete user session (settings page)

--- src/app/settings/security/security-form.tsx
+++ src/app/settings/security/security-form.tsx
@@ -5,8 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Form } from '@/components/ui/form';
 import { toast } from '@/components/ui/use-toast';
-import { Chrome, X } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import SessionCard from './_components/session-card';
 
 const securityFormSchema = z.object({
   add_application: z.boolean().default(false).optional(),
@@ -52,26 +51,11 @@ export function SecurityForm() {
               Devices logged into your account
             </p>
           </div>
-          <div className='group ml-4 flex items-center justify-between space-x-4 rounded-xl bg-muted/60 p-4 transition-colors duration-150 ease-in hover:bg-muted/80'>
-            <div className='flex items-center space-x-4'>
-              <div className='rounded-xl bg-neutral-700 p-2'>
-                <Chrome className='h-5 w-5 text-primary' />
-              </div>
-              <div className='space-y-1'>
-                <p className=' text-sm font-medium leading-none text-primary'>
-                  Microsoft Edge on Windows
-                </p>
-                <p className='flex items-center text-sm text-muted-foreground'>
-                  <div className='mr-1 h-1 w-1 rounded-full bg-green-500' />
-                  <span className='mr-1 text-green-600'>Current session</span>.
-                  Casablanca, MA
-                </p>
-              </div>
-            </div>
-            <Button className='hidden group-hover:flex' size='smi'>
-              <X className='h-4 w-4' />
-            </Button>
-          </div>
+          <SessionCard
+            userBrowser='Chrome'
+            userLocation='Casablanca, Ma'
+            userMachine='Windows'
+          />
         </form>
       </Form>
     </div>
